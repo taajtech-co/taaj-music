8b607c1425a28e14eaae646dc8bb682fe9aa9f53
taajtech-co <66taajtech@gmail.com>
2026-08-05 22:42:27 +0000

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Taaj Music',
+  description: 'Upload and stream music',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+    }
