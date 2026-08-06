19131c686b01f6bea194e73e1587be3fa79fe24c
taajtech-co <66taajtech@gmail.com>
2026-08-06 08:27:14 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,21 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  var saved = localStorage.getItem('taaj-theme');
+                  var theme = saved || 'dark';
+                  document.documentElement.setAttribute('data-theme', theme);
+                } catch (e) {}
+              })();
+            `,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
   );
-    }
+            }
