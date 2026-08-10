4dd50dcf47d0beed87f68dea77068b0fd35d5e61
Abdul Tawwab <66taajtech@gmail.com>
2026-08-10 10:27:35 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,15 @@ import PageTransition from '../components/PageTransition';
 export const metadata = {
   title: 'Taaj Music',
   description: 'Upload and stream music',
+  manifest: '/manifest.json',
+  icons: {
+    icon: [
+      { url: '/favicon-16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -17,6 +26,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
+        <meta name="theme-color" content="#0b0e20" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -40,4 +50,4 @@ export default function RootLayout({ children }) {
       </body>
     </html>
   );
-    }
+            }
